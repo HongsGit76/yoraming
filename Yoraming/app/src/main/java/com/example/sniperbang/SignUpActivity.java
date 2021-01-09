7e880519d852727c44091ec86243dd7ca7b1bedd
yu04038 <69415375+yu04038@users.noreply.github.com>
2021-01-10 05:06:10 +0900

    회원가입화면 spinner 추가(학과명, 현재 학년)

--- Yoraming/app/src/main/java/com/example/sniperbang/SignUpActivity.java
+++ Yoraming/app/src/main/java/com/example/sniperbang/SignUpActivity.java
@@ -4,19 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class SignUpActivity extends AppCompatActivity {
-
-    private TextView tv_major_1;
+    private Spinner spn_major_1, spn_current;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        spn_major_1 = (Spinner)findViewById(R.id.spn_major_1);
+        spn_major_1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                return;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+        spn_current = (Spinner)findViewById(R.id.spn_current);
+        spn_current.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                return;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
 
     }
 }
