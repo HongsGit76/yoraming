513c5daa8d519f329829e6197119d8efa7753fc5
yu04038 <69415375+yu04038@users.noreply.github.com>
2021-01-10 03:41:35 +0900

    회원가입화면

--- Yoraming/app/src/main/java/com/example/sniperbang/SignUpActivity.java
+++ Yoraming/app/src/main/java/com/example/sniperbang/SignUpActivity.java
@@ -3,12 +3,20 @@ package com.example.sniperbang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextView tv_major_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
