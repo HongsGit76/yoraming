e030549bd059d84e560bd58850451f482aefc170
bangjinhyuk <jinhyuik0103@gmail.com>
2021-01-21 01:02:41 +0900

    시험\

--- Yoraming_Back/myapp/app.js
+++ Yoraming_Back/myapp/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ocrRouter = require('./routes/ocr');
 
 var app = express();
 
@@ -18,9 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//port setup
+app.set('port', process.env.PORT || 9000);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ocr',ocrRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
