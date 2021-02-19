e0ef77b46604728741b558c58ea3fb6e4173b6e3
Yongjin Jo <69415375+yu04038@users.noreply.github.com>
2021-02-19 13:12:45 +0900

    하단 바 클릭시 이미지 변경, 이전 ui와 바뀐 것들 수정

--- Yoraming/app/src/main/java/com/example/yoraming/MainActivity.java
+++ Yoraming/app/src/main/java/com/example/yoraming/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.Fragment;
+
+import android.media.Image;
 import android.os.Bundle;
 
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManger;
     private Fragment currentFragment = null;
     private FragmentTransaction transaction;
+    private ImageView tab_home, tab_detail, tab_mypage, tab_yoraming;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +29,42 @@ public class MainActivity extends AppCompatActivity {
         transaction = fragmentManger.beginTransaction();
         transaction.replace(R.id.fragment_container, new HomeFragment()).commitAllowingStateLoss();
 
+        tab_home = (ImageView) findViewById(R.id.tab_home);
+        tab_detail = (ImageView) findViewById(R.id.tab_detail);
+        tab_yoraming = (ImageView) findViewById(R.id.tab_yoraming);
+        tab_mypage = (ImageView) findViewById(R.id.tab_mypage);
+
         View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 transaction = fragmentManger.beginTransaction();
                 switch (v.getId()) {
                     case R.id.tab_home:
+                        tab_home.setSelected(true);
+                        tab_detail.setSelected(false);
+                        tab_yoraming.setSelected(false);
+                        tab_mypage.setSelected(false);
                         currentFragment = new HomeFragment();
                         break;
                     case R.id.tab_detail:
+                        tab_home.setSelected(false);
+                        tab_detail.setSelected(true);
+                        tab_yoraming.setSelected(false);
+                        tab_mypage.setSelected(false);
                         currentFragment = new DetailFragment();
                         break;
-                    case R.id.tab_currentSchedule:
+                    case R.id.tab_yoraming:
+                        tab_home.setSelected(false);
+                        tab_detail.setSelected(false);
+                        tab_yoraming.setSelected(true);
+                        tab_mypage.setSelected(false);
                         currentFragment = new currentScheduleFragment();
                         break;
-                    case R.id.tab_afterSchedule:
+                    case R.id.tab_mypage:
+                        tab_home.setSelected(false);
+                        tab_detail.setSelected(false);
+                        tab_yoraming.setSelected(false);
+                        tab_mypage.setSelected(true);
                         currentFragment = new afterScheduleFragment();
                         break;
                     default:
@@ -50,8 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.tab_home).setOnClickListener(buttonListener);
         findViewById(R.id.tab_detail).setOnClickListener(buttonListener);
-        findViewById(R.id.tab_currentSchedule).setOnClickListener(buttonListener);
-        findViewById(R.id.tab_afterSchedule).setOnClickListener(buttonListener);
+        findViewById(R.id.tab_yoraming).setOnClickListener(buttonListener);
+        findViewById(R.id.tab_mypage).setOnClickListener(buttonListener);
     }
 
 }
