225693185a64af80ec80ad539b0fa4fd43bb4233
KangSeonHo <ksk981214@naver.com>
2021-02-22 18:21:54 +0900

    Revert "Add button, logo resource"
    This reverts commit a4bc55267303ba800c8febf6ae9667e29051929f.

--- Yoraming/app/src/main/java/com/example/yoraming/HomeFragment.java
+++ Yoraming/app/src/main/java/com/example/yoraming/HomeFragment.java
@@ -1,13 +1,14 @@
 package com.example.yoraming;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
-
-import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
     View.OnClickListener buttonlistener;
@@ -36,6 +37,7 @@ public class HomeFragment extends Fragment {
     public void onCreateButton(String text, int imageId) {
         Button mButton = new Button(getActivity());
         ViewGroup.LayoutParams pm = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
         mButton.setText(text);
         mButton.setBackgroundResource(imageId);
         mButton.setLayoutParams(pm);
--- Yoraming/app/src/main/java/com/example/yoraming/MainActivity.java
+++ Yoraming/app/src/main/java/com/example/yoraming/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.yoraming;
 
-import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         transaction = fragmentManger.beginTransaction();
         transaction.replace(R.id.fragment_container, new HomeFragment()).commitAllowingStateLoss();
-        findViewById(R.id.tab_home).setSelected(true);
+
         View.OnClickListener buttonListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,31 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (v.getId()) {
                     case R.id.tab_home:
                         currentFragment = new HomeFragment();
-                        findViewById(R.id.tab_home).setSelected(true);
-                        findViewById(R.id.tab_detail).setSelected(false);
-                        findViewById(R.id.tab_currentSchedule).setSelected(false);
-                        findViewById(R.id.tab_afterSchedule).setSelected(false);
                         break;
                     case R.id.tab_detail:
                         currentFragment = new DetailFragment();
-                        findViewById(R.id.tab_home).setSelected(false);
-                        findViewById(R.id.tab_detail).setSelected(true);
-                        findViewById(R.id.tab_currentSchedule).setSelected(false);
-                        findViewById(R.id.tab_afterSchedule).setSelected(false);
                         break;
                     case R.id.tab_currentSchedule:
                         currentFragment = new currentScheduleFragment();
-                        findViewById(R.id.tab_home).setSelected(false);
-                        findViewById(R.id.tab_detail).setSelected(false);
-                        findViewById(R.id.tab_currentSchedule).setSelected(true);
-                        findViewById(R.id.tab_afterSchedule).setSelected(false);
                         break;
                     case R.id.tab_afterSchedule:
                         currentFragment = new afterScheduleFragment();
-                        findViewById(R.id.tab_home).setSelected(false);
-                        findViewById(R.id.tab_detail).setSelected(false);
-                        findViewById(R.id.tab_currentSchedule).setSelected(false);
-                        findViewById(R.id.tab_afterSchedule).setSelected(true);
                         break;
                     default:
                         return;
@@ -69,4 +53,5 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tab_currentSchedule).setOnClickListener(buttonListener);
         findViewById(R.id.tab_afterSchedule).setOnClickListener(buttonListener);
     }
+
 }
