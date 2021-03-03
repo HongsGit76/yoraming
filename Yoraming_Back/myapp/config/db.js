89865c65fc3ebb40b32408493ab8825896b159f2
bangjinhyuk <jinhyuk0103@gmail.com>
2021-03-03 15:43:46 +0900

    주석 수정

--- Yoraming_Back/myapp/config/db.js
+++ Yoraming_Back/myapp/config/db.js
@@ -6,11 +6,7 @@ var db = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  // host: "13.58.240.144",
-  // port: "3306",
-  // user: "new_master_user",
-  // password: '1234',
-  // database: "yoraming",
+  
 });
 
 db.connect((err) => {
