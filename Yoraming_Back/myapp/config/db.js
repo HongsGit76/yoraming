3adef6f1da77591f536bbe7399565e32de8e3677
bangjinhyuk <jinhyuk0103@gmail.com>
2021-03-04 00:47:02 +0900

    [UPDATE]안드로이드 폴더구조정리

--- Yoraming_Back/myapp/config/db.js
+++ Yoraming_Back/myapp/config/db.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
+console.log(process.env.MYSQL_HOST);
 var db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  
+  // host: process.env.MYSQL_HOST,
+  // port: process.env.MYSQL_PORT,
+  // user: process.env.MYSQL_USER,
+  // password: process.env.MYSQL_PASSWORD,
+  // database: process.env.MYSQL_DATABASE,
+  host: "13.58.240.144",
+  port: "3306",
+  user: "new_master_user",
+  password: "1234",
+  database: "yoraming",
 });
 
 db.connect((err) => {
