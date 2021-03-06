3a3ea6d1c981a3249cc8811b35f2ad99629240b6
Yongjin Jo <69415375+yu04038@users.noreply.github.com>
2021-03-06 17:30:09 +0900

    add sub_major dialog

---
+++ Yoraming/app/src/main/java/com/example/yoraming/UI/addDialog.java
@@ -0,0 +1,14 @@
+package com.example.yoraming.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_dialog);
+    }
+}
