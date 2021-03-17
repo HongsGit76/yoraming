fe4ed51a8a8df2408032e18ac284e9a714847c21
bangjinhyuk <jinhyuk0103@gmail.com>
2021-03-18 03:17:16 +0900

    [UPDATE] 날짜 자동입력 기능 구현

--- Yoraming/app/src/main/java/com/example/yoraming/spec_input_popup_activity.java
+++ Yoraming/app/src/main/java/com/example/yoraming/spec_input_popup_activity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -12,8 +17,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class spec_input_popup_activity extends Activity {
-    TextView tv_add_spec_title,tv_spec_content_1,tv_spec_content_2,tv_spec_content_3;
-    EditText et_spec_content_1, et_spec_content_2, et_spec_content_3;
+    private TextView tv_add_spec_title,tv_spec_content_1,tv_spec_content_2,tv_spec_content_3;
+    private EditText et_spec_content_1, et_spec_content_2, et_spec_content_3;
+    private int textlength;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,50 @@ public class spec_input_popup_activity extends Activity {
             tv_spec_content_1.setText("자격증명");
             tv_spec_content_2.setText("취득일");
             tv_spec_content_3.setText("자격증 기한");
+            et_spec_content_2.setHint("ex) 2021.01.01");
+            // 최대 길이 설정
+            InputFilter[] FilterArray = new InputFilter[1];
+            FilterArray[0] = new InputFilter.LengthFilter(10);
+            et_spec_content_2.setFilters(FilterArray);
+            et_spec_content_2.setInputType(InputType.TYPE_CLASS_NUMBER);
+            et_spec_content_2.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if(et_spec_content_2.isFocusable() && !s.toString().equals("")) {
+                        try{
+                            textlength = et_spec_content_2.getText().toString().length();
+                        }catch (NumberFormatException e){
+                            e.printStackTrace();
+                            return;
+                        }
+
+                        if (textlength == 4 && before != 1) {
+                            et_spec_content_2.setText(et_spec_content_2.getText().toString()+".");
+                            et_spec_content_2.setSelection(et_spec_content_2.getText().length());
+                        }else if (textlength == 7&& before != 1){
+                            et_spec_content_2.setText(et_spec_content_2.getText().toString()+".");
+                            et_spec_content_2.setSelection(et_spec_content_2.getText().length());
+                        }else if(textlength == 5 && !et_spec_content_2.getText().toString().contains(".")){
+                            et_spec_content_2.setText(et_spec_content_2.getText().toString().substring(0,4)+"."+et_spec_content_2.getText().toString().substring(4));
+                            et_spec_content_2.setSelection(et_spec_content_2.getText().length());
+                        }else if(textlength == 8 && !et_spec_content_2.getText().toString().substring(7,8).equals(".")){
+                            et_spec_content_2.setText(et_spec_content_2.getText().toString().substring(0,7)+"."+et_spec_content_2.getText().toString().substring(7));
+                            et_spec_content_2.setSelection(et_spec_content_2.getText().length());
+                        }
+                    }
+
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
 
         }else if(spec_type.equals("career")){
             tv_add_spec_title.setText("경력사항 추가");
@@ -68,6 +119,44 @@ public class spec_input_popup_activity extends Activity {
             //tv_spec_content_3.setText("");
 
         }
+        et_spec_content_3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(et_spec_content_3.isFocusable() && !s.toString().equals("")) {
+                    try{
+                        textlength = et_spec_content_3.getText().toString().length();
+                    }catch (NumberFormatException e){
+                        e.printStackTrace();
+                        return;
+                    }
+
+                    if (textlength == 4 && before != 1) {
+                        et_spec_content_3.setText(et_spec_content_3.getText().toString()+".");
+                        et_spec_content_3.setSelection(et_spec_content_3.getText().length());
+                    }else if (textlength == 7&& before != 1){
+                        et_spec_content_3.setText(et_spec_content_3.getText().toString()+".");
+                        et_spec_content_3.setSelection(et_spec_content_3.getText().length());
+                    }else if(textlength == 5 && !et_spec_content_3.getText().toString().contains(".")){
+                        et_spec_content_3.setText(et_spec_content_3.getText().toString().substring(0,4)+"."+et_spec_content_3.getText().toString().substring(4));
+                        et_spec_content_3.setSelection(et_spec_content_3.getText().length());
+                    }else if(textlength == 8 && !et_spec_content_3.getText().toString().substring(7,8).equals(".")){
+                        et_spec_content_3.setText(et_spec_content_3.getText().toString().substring(0,7)+"."+et_spec_content_3.getText().toString().substring(7));
+                        et_spec_content_3.setSelection(et_spec_content_3.getText().length());
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
